df647b5411efecdde0bb60088368482b8648fb15
zwz <1915967609@qq.com>
2025-01-25 20:20:17 +0800

    feat: complete service registration (consul)

--- services/carts/carts.go
+++ services/carts/carts.go
@@ -10,8 +10,10 @@ import (
 	"jijizhazha1024/go-mall/services/carts/internal/svc"
 
 	"github.com/zeromicro/go-zero/core/conf"
+	"github.com/zeromicro/go-zero/core/logx"
 	"github.com/zeromicro/go-zero/core/service"
 	"github.com/zeromicro/go-zero/zrpc"
+	"github.com/zeromicro/zero-contrib/zrpc/registry/consul"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 )
@@ -32,6 +34,10 @@ func main() {
 			reflection.Register(grpcServer)
 		}
 	})
+	if err := consul.RegisterService(c.ListenOn, c.Consul); err != nil {
+		logx.Errorw("register service error", logx.Field("err", err))
+		panic(err)
+	}
 	defer s.Stop()
 
 	fmt.Printf("Starting rpc server at %s...\n", c.ListenOn)
