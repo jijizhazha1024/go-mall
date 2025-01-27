985043402ae715d2bcf45be38c0c666ffdc8dffa
djj <2482897386@qq.com>
2025-01-27 15:41:25 +0800

    (bug) user 修改error  返回bug

--- test/rpc/users_test.go
+++ test/rpc/users_test.go
@@ -14,7 +14,7 @@ import (
 var users_client users.UsersClient
 
 func initusers() {
-	conn, err := grpc.Dial(fmt.Sprintf("0.0.0.0:%d", biz.UsersRpcPort), grpc.WithBlock(),
+	conn, err := grpc.NewClient(fmt.Sprintf("0.0.0.0:%d", biz.UsersRpcPort), grpc.WithBlock(),
 		grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		panic(err)
