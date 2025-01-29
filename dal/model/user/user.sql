3b666665ae1feda7621748692d3faba5ca713e14
djj <2482897386@qq.com>
2025-01-29 21:28:44 +0800

    （feat） sql   增加登出时间

--- dal/model/user/user.sql
+++ dal/model/user/user.sql
@@ -8,6 +8,7 @@ CREATE TABLE users
     avatar_url    VARCHAR(255) DEFAULT NULL COMMENT '头像图片 URL',
     created_at    TIMESTAMP     DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
     user_deleted  BOOLEAN DEFAULT FALSE COMMENT '用户是否已删除',
+    login_at      TIMESTAMP     DEFAULT NULL COMMENT '最近一次登录时间',
     updated_at    TIMESTAMP     DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
     PRIMARY KEY (user_id)
 );
