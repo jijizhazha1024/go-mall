069f59f9e32689f13d6fcdebedbba0816c233e2b
bbz <2632141215@qq.com>
2025-02-14 09:51:12 +0800

    test: 测试通过id获取优惠卷

--- test/rpc/coupons/coupon_test.go
+++ test/rpc/coupons/coupon_test.go
@@ -17,9 +17,38 @@ func Test_ListCouponsLogic_ListCoupons(t *testing.T) {
 	})
 	if err != nil {
 		t.Error(err)
+		return
 	}
 	assert.Equal(t, uint32(0), resp.StatusCode)
 	for _, coupon := range resp.Coupons {
 		t.Log(coupon)
 	}
+}
+
+// 测试获取优惠券, 优惠券不存在
+func Test_GetCouponLogic_GetCoupon_NotFount(t *testing.T) {
+
+	resp, err := couponsClient.GetCoupon(context.Background(), &coupons.GetCouponReq{
+		Id: "1",
+	})
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	if resp.StatusCode != 0 {
+		t.Logf("code：%d, msg:%s", resp.StatusCode, resp.StatusMsg)
+		return
+	}
+
+}
+func Test_GetCouponLogic_GetCoupon(t *testing.T) {
+
+	resp, err := couponsClient.GetCoupon(context.Background(), &coupons.GetCouponReq{
+		Id: "67508ec1ea7111ef86d80242ac120005",
+	})
+	if err != nil {
+		t.Error(err)
+	}
+	assert.Equal(t, uint32(0), resp.StatusCode)
+	t.Log(resp.Coupon)
 }
