912f0152441b1bdff18a90befe774f303818d1c5
djj <2482897386@qq.com>
2025-02-20 16:00:33 +0800

    (feat)订单服务

---
+++ dal/model/order/order_addresses/vars.go
@@ -0,0 +1,5 @@
+package order_addresses
+
+import "github.com/zeromicro/go-zero/core/stores/sqlx"
+
+var ErrNotFound = sqlx.ErrNotFound
---
+++ dal/model/order/order_items/vars.go
@@ -0,0 +1,5 @@
+package order_items
+
+import "github.com/zeromicro/go-zero/core/stores/sqlx"
+
+var ErrNotFound = sqlx.ErrNotFound
