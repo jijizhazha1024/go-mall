b76e3ec8521cf97289ac6ac085f956d52fd7c32e
zwz <1915967609@qq.com>
2025-02-20 21:11:02 +0800

    feat: add env parameter

--- services/checkout/internal/config/config.go
+++ services/checkout/internal/config/config.go
@@ -1,7 +1,17 @@
 package config
 
-import "github.com/zeromicro/go-zero/zrpc"
+import (
+	"github.com/zeromicro/go-zero/zrpc"
+	"github.com/zeromicro/zero-contrib/zrpc/registry/consul"
+)
 
 type Config struct {
 	zrpc.RpcServerConf
+	MysqlConfig MysqlConfig
+	Consul      consul.Conf
+}
+
+type MysqlConfig struct {
+	DataSource  string
+	Conntimeout int
 }
--- services/checkout/internal/svc/servicecontext.go
+++ services/checkout/internal/svc/servicecontext.go
@@ -1,13 +1,23 @@
 package svc
 
-import "jijizhazha1024/go-mall/services/checkout/internal/config"
+import (
+	"github.com/zeromicro/go-zero/core/stores/sqlx"
+	"jijizhazha1024/go-mall/dal/model/checkout"
+	"jijizhazha1024/go-mall/services/checkout/internal/config"
+	"jijizhazha1024/go-mall/services/checkout/internal/db"
+)
 
 type ServiceContext struct {
-	Config config.Config
+	Config        config.Config
+	Mysql         sqlx.SqlConn
+	CheckoutModel checkout.CheckoutsModel
 }
 
-func NewServiceContext(c config.Config) *ServiceContext {
+func NewServiceContext(c config.Config) (*ServiceContext, error) {
+	mysql := db.NewMysql(c.MysqlConfig)
 	return &ServiceContext{
-		Config: c,
-	}
+		Config:        c,
+		Mysql:         mysql,
+		CheckoutModel: checkout.NewCheckoutsModel(mysql),
+	}, nil
 }
