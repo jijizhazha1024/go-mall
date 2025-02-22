f6f601ad687fee0c8580f6c5121cea72878fa536
djj <2482897386@qq.com>
2025-02-22 12:14:12 +0800

    (merge dev)

--- dal/model/inventory/inventory.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE inventory
-(
-    product_id INT, -- 与商品服务共享同一ID
-    total      INT NOT NULL,
-    sold       INT NOT NULL,
-    primary key (product_id)
-);
--- dal/model/inventory/vars.go
+++
@@ -1,5 +0,0 @@
-package inventory
-
-import "github.com/zeromicro/go-zero/core/stores/sqlx"
-
-var ErrNotFound = sqlx.ErrNotFound
