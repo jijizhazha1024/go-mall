3d4fc13bde27982835585f64d3339ecd8cf98d32
bbz <2632141215@qq.com>
2025-02-22 19:52:37 +0800

    feat: 添加订单对支付成功调用失败补偿

--- common/consts/code/order.go
+++ common/consts/code/order.go
@@ -3,9 +3,11 @@ package code
 const (
 	OrderNotExist = 11000 + iota
 	OrderStatusInvalid
+	PaymentStatusInvalid
 )
 
 const (
-	OrderNotExistMsg      = "订单不存在"
-	OrderStatusInvalidMsg = "订单状态无效"
+	OrderNotExistMsg        = "订单不存在"
+	OrderStatusInvalidMsg   = "订单状态无效"
+	PaymentStatusInvalidMsg = "订单支付状态无效"
 )
